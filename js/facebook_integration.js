793ad190f64a578eb39d0d960e126399888309a6
Amir <pascalazubi@gmail.com>
2021-08-03 23:56:55 +0200

    New feature: Individual App ID
    - Give the user the possibility to add in his App ID
    - Give the user the possibility to reopen the Facebook Login

--- js/facebook_integration.js
+++ js/facebook_integration.js
@@ -2,78 +2,78 @@
 let accessToken; // Access Token
 let act_id; // Ad Account ID
 
-
 // Filter auswählen (Week,Month,Year)
-const filter = document.getElementById('filter')
+const filter = document.getElementById("filter");
 
 // Optionen für den Chart (Wechseln: https://apexcharts.com/docs/)
 var options = {
   chart: {
     height: 350,
-    type: 'area',
-    curve: 'smooth',
+    type: "area",
+    curve: "smooth",
   },
   dataLabels: {
-    enabled: false
+    enabled: false,
   },
   series: [],
   title: {
-    text: 'Amount Spent',
+    text: "Amount Spent",
   },
   noData: {
-    text: 'Please use a filter to get data.'
+    text: "Please use a filter to get data.",
   },
   markers: {
     size: 5,
     colors: ["#544cf2"],
     strokeColor: "#544cf2",
-    strokeWidth: 3
-  }
-}
+    strokeWidth: 3,
+  },
+};
 
 // Chart erstellen (Mit den definierten Optionen aus der Variable "options") und mit der index.html verbinden
-var chart = new ApexCharts(
-  document.querySelector("#chart"),
-  options
-);
+var chart = new ApexCharts(document.querySelector("#chart"), options);
 
 // Chart Laden
 chart.render();
+
+// appID Abfrage (Prompt)
+var appID = prompt("Add your App ID");
 
 // Integration von Facebook mit der zu verknüpfenden App ID
 window.fbAsyncInit = function () {
   console.log();
   FB.init({
-    appId: '1038413933267656',
+    appId: appID,
     autoLogAppEvents: true,
     xfbml: true,
-    version: 'v9.0'
+    version: "v9.0",
+    status: true,
   });
   // Integration von Facebook Login
   FB.login(function (response) {
     UserID = response.authResponse.userID;
     if (response.authResponse) {
       // Antwort von Facebook wenn es funtkioniert hat
-      console.log('Herzlich willkommen!  Du wurdest erfolgreich eingeloggt. ');
+      console.log("Welcome! You have been logged in successfully.");
 
-      FB.api('/me/adaccounts', function (response) {
+      FB.api("/me/adaccounts", function (response) {
         // 1. https://developers.facebook.com/tools/explorer/?method=GET&path=me%2Fadaccounts&version=v9.0
         // 2. Rechte vergeben
         // 3. Gewünschter Ad Account auswählen, welcher die Daten enthält
         // 4. Nummer dann anpassen response.data[Zahl hier anpassen]
-        act_id = response.data[1].id
+        act_id = response.data[1].id;
         getData(act_id);
       });
     }
     // wenn das Login fehlschlägt
     else {
-      console.log('Benutzer hat die Anmeldung abgebrochen oder ist nicht vollständig autorisiert.');
+      console.log("User canceled login or is not fully authorized.");
     }
   });
 
   // Für jeden Request einen Token bekommen
   FB.getLoginStatus(function (response) {
-    if (response.status === 'connected') {
+    if (response.status === "connected") {
       // Speichern des Zugriffstokens in accessToken, mit dem wir eine Datenanforderung an den Facebook-Server stellen
       accessToken = response.authResponse.accessToken;
     }
@@ -83,111 +83,121 @@ window.fbAsyncInit = function () {
   const getData = async (actid) => {
     // Anfrage an den Server stellen, um Klicks, Impressionen, Ausgaben, Kontowährung usw. abzurufen
     // Der "await"-Ausdruck lässt async-Funktionen pausieren, bis ein Promise erfüllt ist
-    const response = await fetch(`https://graph.facebook.com/v9.0/${actid}/insights?fields=impressions,clicks,spend,account_currency&access_token=${accessToken}`);
-    // Nach der Anfrage an die Server erhalten wir die geforderten Daten von Facebook und speichern diese in data 
-    const data = await response.json()
+    const response = await fetch(
+      `https://graph.facebook.com/v9.0/${actid}/insights?fields=impressions,clicks,spend,account_currency&access_token=${accessToken}`
+    );
+    // Nach der Anfrage an die Server erhalten wir die geforderten Daten von Facebook und speichern diese in data
+    const data = await response.json();
     // Aus dem Datenobjekt erhalten wir nur die folgenden Werte --> Impressionen, Klicks, Ausgaben, Kontowährung
-    const { impressions, clicks, spend, account_currency } = data.data[0]
+    const { impressions, clicks, spend, account_currency } = data.data[0];
     // Umformatierung der erhaltenen Daten
-    let impressionC = numberWithCommas(impressions)
-    let clicksC = numberWithCommas(clicks)
+    let impressionC = numberWithCommas(impressions);
+    let clicksC = numberWithCommas(clicks);
     // Übertragung der Daten in die index Datei
-    $('#amount').text(spend)
-    $('#impression').text(impressionC)
-    $('#clicks').text(clicksC)
-    $('#currency').text(account_currency)
-  }
+    $("#amount").text(spend);
+    $("#impression").text(impressionC);
+    $("#clicks").text(clicksC);
+    $("#currency").text(account_currency);
+  };
 
   // Hilfsfunktion für die Umformatierung
   const numberWithCommas = (x) => {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  }
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
 
   // Damit sich die Daten anpassen wenn der User den Dropdown verwendet
-  filter.addEventListener('change', (event) => {
+  filter.addEventListener("change", (event) => {
     // Updaten der Daten nach den gewählten Kriterien vom User
-    updateData(event.target.value) //Wertübergabe an die Funktion Werte können Jahr, Monat, Woche sein
+    updateData(event.target.value); //Wertübergabe an die Funktion Werte können Jahr, Monat, Woche sein
 
     // Abfrage des FB-Servers, um den ausgegebenen Betrag aus dem vom Benutzer ausgewählten Wert zu erhalten + Zeitraum wird immer um 1 erhöht
     FB.api(
       `/${act_id}/insights?fields=spend&date_preset=${event.target.value}&time_increment=1`,
-      'GET',
-      { "date_preset": `${event.target.value}` },
-      response => {
+      "GET",
+      { date_preset: `${event.target.value}` },
+      (response) => {
         // nachdem wir die Daten erhalten haben, müssen wir das Diagramm aktualisieren.
         chart.updateOptions({
           xaxis: {
-            categories: response.data.map(val => val.date_start)
-          }
-        })
+            categories: response.data.map((val) => val.date_start),
+          },
+        });
         // Dadurch werden die Daten im Diagramm aktualisiert
-        chart.updateSeries([{
-          data: response.data.map(val => val.spend)
-        }])
+        chart.updateSeries([
+          {
+            data: response.data.map((val) => val.spend),
+          },
+        ]);
 
         // Wenn wir mehr als als 25 Datensätze erhalten wird es schwieriger zum auf dem Chart darzustellen darum haben wir einen Button hinzugefügt welche den nächsten Datensatz zeigt
         if (response.paging.next) {
           // hier haben wir definiert ob der Button ausgeblendet oder eingeblendet sein soll
-          $('#next').css('visibility', 'visible')
+          $("#next").css("visibility", "visible");
           // wenn der User auf den Button drückt wird die funktion getDatafromserver ausgeführt
-          $('#next').click(() => {
+          $("#next").click(() => {
             // anfrage erstellen um die Daten zu erhalten
-            getDataFromServer(response.paging.next)
-          })
+            getDataFromServer(response.paging.next);
+          });
         }
       }
     );
-  })
+  });
 
   // Funktion um die Daten abzurufen
   const getDataFromServer = (url) => {
-
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-
+      .then((response) => response.json())
+      .then((data) => {
         // nachdem wir einen weiteren Datensatz erhalten haben, aktualisieren wir das Diagramm
-        updateChart(data)
-        // das gleiche gilt, wenn wir mehr als diese 25 Daten haben 
+        updateChart(data);
+        // das gleiche gilt, wenn wir mehr als diese 25 Daten haben
         if (data.paging.next) {
-          $('#next').click(() => {
-            getDataFromServer(data.paging.next)
-          })
+          $("#next").click(() => {
+            getDataFromServer(data.paging.next);
+          });
         } else {
           // wenn es keine Daten hat oder weniger als 25 dann wird der Button nicht angezeigt
-          $('#next').css('visibility', 'hidden')
+          $("#next").css("visibility", "hidden");
         }
-      })
-  }
+      });
+  };
 
   // Funktion zum Aktualisieren des Diagramms
   const updateChart = (data) => {
     // Dadurch werden sowohl die x- als auch die y-Achse des Diagramms aktualisiert
     chart.updateOptions({
-      series: [{
-        data: data.data.map(val => val.spend)
-      }],
+      series: [
+        {
+          data: data.data.map((val) => val.spend),
+        },
+      ],
       xaxis: {
-        categories: data.data.map(val => val.date_start)
-      }
-    })
-  }
+        categories: data.data.map((val) => val.date_start),
+      },
+    });
+  };
 
   // Funktion zum Aktualisieren der Daten oberhalb des Charts --> also amount, impressions, clicks
   const updateData = async (filterVal) => {
-
     FB.api(
       `/${act_id}/insights?fields=clicks,impressions,spend,account_currency`,
-      'GET',
-      { "date_preset": `${filterVal}` },
-      response => {
-        const { spend, impressions, clicks, account_currency } = response.data[0]
+      "GET",
+      { date_preset: `${filterVal}` },
+      (response) => {
+        const { spend, impressions, clicks, account_currency } =
+          response.data[0];
         // Hinzufügen von Daten zur index Datei + Umformatierung dieser
-        $('#amount').text(spend.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))
-        $('#impression').text(impressions.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))
-        $('#clicks').text(clicks.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','))
-        $('#currency').text(account_currency)
+        $("#amount").text(
+          spend.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        );
+        $("#impression").text(
+          impressions.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        );
+        $("#clicks").text(
+          clicks.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        );
+        $("#currency").text(account_currency);
       }
     );
-  }
+  };
 };
